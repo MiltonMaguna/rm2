842fe9b9ad4d8e4571742d65a1d8653f85891591
Milton Maguna <milton.maguna@gmail.com>
2025-09-23 16:52:57 -0300

    Refactor import paths and update docstrings in backdrops.py; standardize string quotes and improve code clarity

--- render_manager/render/libs/helpers/backdrops.py
+++ render_manager/render/libs/helpers/backdrops.py
@@ -3,27 +3,28 @@
 # Maximiliano Rocamora / Milton Maguna
 # ----------------------------------------------------------------------------------------
 import contextlib
+
 try:
     import nuke
     import nukescripts
 except ImportError:
     import render_manager.mocks.nuke as nuke
-from render_manager.render.render_layer_types import Render
-from render_manager.render.libs.helpers.reads import create_all_aovs
-from render_manager.render.libs.helpers.config import BACKDROP_SIZE, ROL_POSITION_X
-from render_manager.render.libs.helpers.config import OFFSET_BDROP_X, OFFSET_BDROP_Y
-from render_manager.render.libs.helpers.config import COLOR_BACKDROP_RL
+from rm2.render_manager.render.render_layer_types import Render
+from rm2.render_manager.render.libs.helpers.reads import create_all_aovs
+from rm2.render_manager.render.libs.helpers.config import BACKDROP_SIZE, ROL_POSITION_X
+from rm2.render_manager.render.libs.helpers.config import OFFSET_BDROP_X, OFFSET_BDROP_Y
+from rm2.render_manager.render.libs.helpers.config import COLOR_BACKDROP_RL
 
 
 def get_next_row_container(render: Render) -> int:
-    ''' Get the maximum row number of the backdrop node for this layer in the script. '''
+    """Get the maximum row number of the backdrop node for this layer in the script."""
 
     same_name_backdrops = []  # Lista para almacenar los backdrops con el mismo nombre
 
-    for backdrop in nuke.allNodes('BackdropNode'):
+    for backdrop in nuke.allNodes("BackdropNode"):
         try:
-            if backdrop['container'].getValue():
-                if render.rol_main() == backdrop['rol_main'].getValue():
+            if backdrop["container"].getValue():
+                if render.rol_main() == backdrop["rol_main"].getValue():
                     same_name_backdrops.append(backdrop)
         except (NameError, AttributeError):
             continue
@@ -32,39 +33,43 @@ def get_next_row_container(render: Render) -> int:
 
 
 def get_next_row_subcontainer(render: Render) -> int:
-    ''' Get the maximum row number of the backdrop node for this layer in the script. '''
+    """Get the maximum row number of the backdrop node for this layer in the script."""
 
-    for backdrop in nuke.allNodes('BackdropNode'):
+    for backdrop in nuke.allNodes("BackdropNode"):
         try:
-            if backdrop['container'].getValue():
-                if render.rol_layer() == backdrop['rol_layer'].getValue():
-                    return int(backdrop['row'].getValue())
+            if backdrop["container"].getValue():
+                if render.rol_layer() == backdrop["rol_layer"].getValue():
+                    return int(backdrop["row"].getValue())
         except (NameError, AttributeError):
             continue
 
 
 def get_max_row_backrops(same_name_backdrops: list) -> int:
-    ''' Get the maximum row number of the backdrop node for this layer in the script. '''
+    """Get the maximum row number of the backdrop node for this layer in the script."""
     if same_name_backdrops:
         # Obtener la fila máxima entre los backdrops con el mismo nombre
         max_row = max(
-            int(backdrop['row'].getValue()) for backdrop in same_name_backdrops)
+            int(backdrop["row"].getValue()) for backdrop in same_name_backdrops
+        )
         return max_row + 1
     else:
         return 1
 
 
 def create_backdrop_container(render: Render, row: int) -> str:
-    ''' Create backdrop container for all renders '''
+    """Create backdrop container for all renders"""
 
     # check in nuke if a backdrop node for this Render already exists
-    for n in nuke.allNodes('BackdropNode'):
+    for n in nuke.allNodes("BackdropNode"):
         with contextlib.suppress(NameError):
-            if n['rol_layer'].getValue() == render.rol_layer() and n['container'].getValue():
+            if (
+                n["rol_layer"].getValue() == render.rol_layer()
+                and n["container"].getValue()
+            ):
                 return n
 
-    node = _create_backdrop(render.prefix_rol_layer(), BACKDROP_SIZE['GENERAL'])
-    node['tile_color'].setValue(COLOR_BACKDROP_RL[render.rol_main()])
+    node = _create_backdrop(render.prefix_rol_layer(), BACKDROP_SIZE["GENERAL"])
+    node["tile_color"].setValue(COLOR_BACKDROP_RL[render.rol_main()])
     _add_attributes_tab(node, render, row, container=True)
     _move_backdrop(node)
 
@@ -72,14 +77,16 @@ def create_backdrop_container(render: Render, row: int) -> str:
 
 
 def create_backdrop_subcontainer(render: Render, row: int):
-    ''' Create backdrop for all renders'''
+    """Create backdrop for all renders"""
 
     connections = {}
 
-    for bd in nuke.allNodes('BackdropNode'):
+    for bd in nuke.allNodes("BackdropNode"):
         with contextlib.suppress(NameError):
-            if bd["name_layer"].getValue() == render.name() and bd['subcontainer'].getValue():
-
+            if (
+                bd["name_layer"].getValue() == render.name()
+                and bd["subcontainer"].getValue()
+            ):
                 for node in bd.getNodes():
                     # Save current connections in dict
                     dependent_node = []
@@ -91,7 +98,7 @@ def create_backdrop_subcontainer(render: Render, row: int):
                     nuke.delete(node)
 
                 # overwrite current row before delete the subcontainer
-                row = int(bd['row'].getValue())
+                row = int(bd["row"].getValue())
                 nuke.delete(bd)
 
     for backdrop_type in BACKDROP_SIZE:
@@ -103,15 +110,16 @@ def create_backdrop_subcontainer(render: Render, row: int):
 
             # restore connections
             for read in connections:
-
                 for _node in connections[read]:
                     _node.setInput(0, nuke.toNode(read))
 
 
-def _add_attributes_tab(node, render: Render, row: int, container: bool = False) -> None:
+def _add_attributes_tab(
+    node, render: Render, row: int, container: bool = False
+) -> None:
     # sourcery skip: extract-duplicate-method
-    ''' Create Arcane tab with knobs attributes '''
-    Arcane_tab = nuke.Tab_Knob('ARCANE')
+    """Create Arcane tab with knobs attributes"""
+    Arcane_tab = nuke.Tab_Knob("ARCANE")
     node.addKnob(Arcane_tab)
 
     rol_main = nuke.String_Knob("rol_main", "rol_main")
@@ -165,7 +173,7 @@ def _add_attributes_tab(node, render: Render, row: int, container: bool = False)
 
 
 def _move_backdrop(backdrop_node: str) -> None:
-    ''' Custom position for backdrop '''
+    """Custom position for backdrop"""
 
     backdrop = nuke.toNode(backdrop_node.name())
     column = int(backdrop["column"].getValue())
@@ -175,12 +183,11 @@ def _move_backdrop(backdrop_node: str) -> None:
     node_in_backdrop.append(backdrop_node)
 
     for node in node_in_backdrop:
+        _xpos = int(node["xpos"].getValue())
+        _ypos = int(node["ypos"].getValue())
 
-        _xpos = int(node['xpos'].getValue())
-        _ypos = int(node['ypos'].getValue())
-
-        xpos_custom = (OFFSET_BDROP_X * column)
-        ypos_custom = (OFFSET_BDROP_Y * row)
+        xpos_custom = OFFSET_BDROP_X * column
+        ypos_custom = OFFSET_BDROP_Y * row
 
         node.setXYpos(_xpos + xpos_custom, _ypos + ypos_custom)
 
@@ -188,16 +195,17 @@ def _move_backdrop(backdrop_node: str) -> None:
 
 
 def _create_backdrop(backdrop_name: str, backdrop_size: dict):
-    ''' creates and returns a backdrop node, sets pos/size/label attributes '''
+    """creates and returns a backdrop node, sets pos/size/label attributes"""
 
-    node = nuke.nodes.BackdropNode(label=backdrop_name,
-                                   xpos=backdrop_size['xpos'],
-                                   bdwidth=backdrop_size['bdwidth'],
-                                   ypos=backdrop_size['ypos'],
-                                   bdheight=backdrop_size['bdheight'],
-                                   tile_color=backdrop_size['tile_color'],
-                                   z_order=backdrop_size['z_order'],
-                                   note_font_size=backdrop_size['note_font_size']
-                                   )
+    node = nuke.nodes.BackdropNode(
+        label=backdrop_name,
+        xpos=backdrop_size["xpos"],
+        bdwidth=backdrop_size["bdwidth"],
+        ypos=backdrop_size["ypos"],
+        bdheight=backdrop_size["bdheight"],
+        tile_color=backdrop_size["tile_color"],
+        z_order=backdrop_size["z_order"],
+        note_font_size=backdrop_size["note_font_size"],
+    )
 
     return node
