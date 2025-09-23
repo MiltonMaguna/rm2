e2bd588e7e3692b542e802f1959c37631159476d
Milton Maguna <milton.maguna@gmail.com>
2025-09-23 16:49:37 -0300

    Refactor import paths and standardize string quotes in test_send_python_job.py and test_send_python_job_proxy.py

--- render_manager/core/deadline/test_send_python_job.py
+++ render_manager/core/deadline/test_send_python_job.py
@@ -1,31 +1,31 @@
 # example file on how to send a cache export job via command line
 import os
 
-from render_manager.core.deadline.python_deadline import PythonDeadline
+from rm2.render_manager.core.deadline.python_deadline import PythonDeadline
 
 path = "F:/nuke/arcane/dev/RenderManager/render_manager/core/deadline/job_scripts"
-SCRIPT = os.path.join(path, 'reformat_oiio.py')
+SCRIPT = os.path.join(path, "reformat_oiio.py")
 
 
 args = {
-    'frame_range': '999-1004',
-    'resize': '50%',
-    'input_file': r"G:/Temp/nuke/test_oiio/BTY/RND_BG_BTY_beauty_%04d.exr",
-    'output_file': r"G:/Temp/nuke/test_oiio/BTY_REFORMAT/RND_BG_BTY_beauty_%04d.exr",
-    'filter': 'cubic',
+    "frame_range": "999-1004",
+    "resize": "50%",
+    "input_file": r"G:/Temp/nuke/test_oiio/BTY/RND_BG_BTY_beauty_%04d.exr",
+    "output_file": r"G:/Temp/nuke/test_oiio/BTY_REFORMAT/RND_BG_BTY_beauty_%04d.exr",
+    "filter": "cubic",
 }
 
 info = {
-    'Name': 'test-ref2',
-    "BatchName": 'Test_NukeReformat_oiio',
+    "Name": "test-ref2",
+    "BatchName": "Test_NukeReformat_oiio",
 }
 
 
 def submit():
-    ''' Runs job '''
-    dead = PythonDeadline('job_display_name')
+    """Runs job"""
+    dead = PythonDeadline("job_display_name")
     dead.run_job(info, args, SCRIPT)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     submit()
