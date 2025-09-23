49685c70f2213799211f2b2bd3b403e55e0c0f83
Milton Maguna <milton.maguna@gmail.com>
2025-09-23 16:50:56 -0300

    Refactor MODEL_DATA structure and standardize string quotes in config.py

--- render_manager/mvc/config.py
+++ render_manager/mvc/config.py
@@ -4,20 +4,17 @@
 
 # column order, label and width
 MODEL_DATA = [
-    (0, 'layer', 140),  # name of the render layer
-    (1, 'nuke version', 80),  # scene layer, version
-    (2, 'nuke range', 80),  # current nuke read range
-    (3, 'nuke frames', 80),  # frames count
-    (4, 'status', 105),  # icon/scene status
-    (5, 'version', 70),  # last layer, version
-    (6, 'range', 80),  # current disk layer range
-    (7, 'frames', 70),  # frames count
-    (8, 'aov', 60),  # aovs count
+    (0, "layer", 170),  # name of the render layer
+    (1, "version", 80),  # scene layer, version
+    (2, "range", 80),  # current nuke read range
+    (3, "frames", 80),  # frames count
+    (4, "aovs", 60),  # aovs count
+    (5, "status", 105),  # icon/scene status
 ]
 
 # empty dict with text blocks for displayRole in modelview
-MODEL_DISPLAYROLE = {i: '---' for i in range(len(MODEL_DATA))}
+MODEL_DISPLAYROLE = {i: "---" for i in range(len(MODEL_DATA))}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(MODEL_DISPLAYROLE)
